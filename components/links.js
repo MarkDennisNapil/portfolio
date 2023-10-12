28a7efb24e21d235a0f2537effe089261205db32
Mark Dennis Napil <120807518+MarkDennisNapil@users.noreply.github.com>
2023-10-12 17:33:00 +0800

    Update links.js

--- components/links.js
+++ components/links.js
@@ -15,7 +15,7 @@ const Links = () => {
             <Link href="/contact" className={styles.linktext}><Image src={phone} className={styles.icon} alt={name} /><label>+639463964124</label></Link>
             <Link href="https://web.facebook.com/markdennis.napil" className={styles.linktext}><Image src={fb} className={styles.icon} alt={name} /><label>Facebook</label></Link>
             <Link href="https://www.instagram.com/markdennis_1/" className={styles.linktext}><Image src={instagram} className={styles.icon} alt={name} /><label>Instagram</label></Link>
-            <Link href="https://twitter.com/MarkDen43990737" className={styles.linktext}><Image src={twitter} className={styles.icon} alt={name} /><label>TwitterX</label></Link>
+            <Link href="https://twitter.com/MarkDen43990737" className={styles.linktext}><Image src={twitter} className={styles.icon} alt={name} /><label>X</label></Link>
         </div>
     )
 }
